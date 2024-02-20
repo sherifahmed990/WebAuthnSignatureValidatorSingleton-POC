8ef0d70e8df69ef8f9f661dd187918701f473091
Sherif Abdelmoatty <sherif.ahmed990@gmail.com>
2024-02-21 00:01:59 +0300

    WebAuthnSignatureValidatorSingleton poc

---
+++ script/DeployScript.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Script.sol";
+import {WebAuthnSignatureValidatorSingleton} from "../src/WebAuthnSignatureValidatorSingleton.sol";
+import {Create2} from "@openzeppelin/contracts/utils/Create2.sol";
+
+contract DeterministicDeployScript is Script {
+    //https://github.com/Zoltu/deterministic-deployment-proxy/blob/master/source/deterministic-deployment-proxy.yul
+    address internal constant DETERMINISTIC_CREATE2_FACTORY = 0x7A0D94F55792C434d74a40883C6ed8545E406D12;
+    address internal constant EXPECTED_ADDRESS = 0xcA66C5A0eEAb0Fe74F343bb4A539042c68aE45F9;
+
+    function run() public 
+    returns (WebAuthnSignatureValidatorSingleton webAuthnSigner){
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        bytes memory returnData;
+        bool success;
+
+        if(!isContract(EXPECTED_ADDRESS)){ //check if already deployed
+            bytes memory managerCreationCode = abi.encodePacked(
+            type(WebAuthnSignatureValidatorSingleton).creationCode, abi.encode(0xCAc51aDF726E4b269645a7fD6a43296A1Ff53e8d));
+
+            (success, returnData) = DETERMINISTIC_CREATE2_FACTORY.call(
+                managerCreationCode);
+            webAuthnSigner = WebAuthnSignatureValidatorSingleton(address(uint160(bytes20(returnData))));
+
+            assert(address(webAuthnSigner) == EXPECTED_ADDRESS);
+        }
+        vm.stopBroadcast();
+    }
+
+    function isContract(address addr) internal view returns (bool) {
+        uint size;
+        assembly { size := extcodesize(addr) }
+        return size > 0;
+    }
+}
---
+++ src/WebAuthnSignatureValidatorSingleton.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+/* solhint-disable one-contract-per-file */
+pragma solidity >=0.8.0;
+
+import {SignatureValidator} from "./SignatureValidator.sol";
+import {IWebAuthnVerifier, WebAuthnConstants} from "./verifiers/WebAuthnVerifier.sol";
+import {ISafe} from "./interfaces/ISafe.sol";
+
+struct SignatureData {
+    bytes authenticatorData;
+    bytes clientDataFields;
+    uint256[2] rs;
+}
+
+/**
+ * @title WebAuthnSignatureValidatorSingleton
+ * @dev A contract that verifies a WebAuthn signature.
+ * https://github.com/safe-global/safe-modules/blob/main/modules/4337/contracts/experimental/WebAuthnSingletonSigner.sol
+ */
+contract WebAuthnSignatureValidatorSingleton is SignatureValidator {
+    IWebAuthnVerifier public immutable WEBAUTHN_SIG_VERIFIER;
+    address private immutable SIGNATURE_VALIDATOR_SINGLETON;
+
+    /**
+     * @dev Constructor function.
+     * @param webAuthnVerifier The address of the P256Verifier contract.
+     */
+    constructor(address webAuthnVerifier) {
+        WEBAUTHN_SIG_VERIFIER = IWebAuthnVerifier(webAuthnVerifier);
+        SIGNATURE_VALIDATOR_SINGLETON = address(this);
+    }
+
+    /**
+     * @inheritdoc SignatureValidator
+     */
+    function _verifySignature(bytes32 message, bytes calldata signature) internal view virtual override returns (bool isValid) {
+        uint256 X;
+        uint256 Y;
+        SignatureData calldata signaturePointer;
+        // solhint-disable-next-line no-inline-assembly
+        assembly ("memory-safe") {
+            signaturePointer := signature.offset
+        }
+
+        (X, Y) = abi.decode(
+            ISafe(msg.sender).getStorageAt(uint256(uint160(SIGNATURE_VALIDATOR_SINGLETON)), 2),
+            (uint256, uint256)
+        );
+
+        return
+            WEBAUTHN_SIG_VERIFIER.verifyWebAuthnSignatureAllowMalleability(
+                signaturePointer.authenticatorData,
+                WebAuthnConstants.AUTH_DATA_FLAGS_UV,
+                message,
+                signaturePointer.clientDataFields,
+                signaturePointer.rs,
+                X,
+                Y
+            );
+    }
+
+    /*
+     *@dev to be delegate called from the Safe account
+     */
+    function setSigner(uint256 X, uint256 Y) public {
+        require(address(this) != SIGNATURE_VALIDATOR_SINGLETON, "setSigner should only be called via delegatecall");
+
+        bytes32 slotX = bytes32(uint256(uint160(SIGNATURE_VALIDATOR_SINGLETON)));
+        bytes32 slotY = bytes32(uint256(slotX)+1);
+        /* solhint-disable no-inline-assembly */
+        /// @solidity memory-safe-assembly
+        assembly {
+            sstore(slotX, X)
+            sstore(slotY, Y)
+        }
+    }
+
+    /*
+     *@dev to be delegate called from the Safe account
+     */
+    function removeSigner() public {
+        require(address(this) != SIGNATURE_VALIDATOR_SINGLETON, "removeSigner should only be called via delegatecall");
+        require(!ISafe(address(this)).isOwner(SIGNATURE_VALIDATOR_SINGLETON), "can't clear the signer storage before removing the validator singleton from the owners list");
+
+        bytes32 slotX = bytes32(uint256(uint160(SIGNATURE_VALIDATOR_SINGLETON)));
+        bytes32 slotY = bytes32(uint256(slotX)+1);
+        /* solhint-disable no-inline-assembly */
+        /// @solidity memory-safe-assembly
+        assembly {
+            sstore(slotX, 0)
+            sstore(slotY, 0)
+        }
+    }
+}
